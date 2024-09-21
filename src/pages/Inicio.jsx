4423c7b340016e0903a426258c5f239fcf3afe3b
Juli-GR <juligarciarosset@gmail.com>
2024-09-21 02:11:22 -0300

    name field and validity check

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'wouter';
 
 function Inicio() {
+  const [name, setName] = useState('');
   const [, navigate] = useLocation();
 
-  const goToListapartidas = () => {
-    navigate('/ListaPartidas');
+  const handleSubmit = (e) => {
+
+    // Chech Regex before chaning the page
+    if (/^[a-zA-Z0-9]{1,10}$/.test(name)) {
+      navigate('/ListaPartidas');
+
+    // Invalid name: display a help message
+    } else {
+      e.preventDefault();
+      document.getElementById("invalid-name").style.display = 'block';
+    }
   };
 
   return (
     <div className="container">
       <h1>EL SWITCHER</h1>
-      <button className="button" onClick={goToListapartidas}>Continuar</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder='name'
+          value={name}
+          onChange={e => setName(e.target.value)}/>
+        <div
+          className="infoLabel"
+          id="invalid-name" >
+            Ingrese un nombre alfanumerico de hasta 10 caracteres
+        </div>
+        <button
+          className="button">
+            Continuar
+        </button>
+      </form>
     </div>
   );
 }
