20a9229387adf81226703b8a6cacdfc68a6808bb
Matteo Mariano <matteomariano10@mi.unc.edu.ar>
2024-09-22 00:35:18 -0300

    First implementation for waitroom

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { Route, Router } from 'wouter';
+import { navigate } from 'wouter/use-browser-location';
+import WaitRoom from './pages/WaitRoom';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // hardcodeado para testear
+  function handleClick(){
+    navigate('/SalaDeEspera/:id')
+  }
+
+  // hardcodeado para testear
+  const players_list = [
+    "juanceto01",
+    "Juli",
+    "Pepito",
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app-container'>
+      <Route path="/">
+        <button onClick={handleClick} >entrar a partida</button>
+      </Route>
+      {/* necesita un id, el tema es que toma cualquier cosa tmb (no pude ponerle regex), nec un entero (supongo que las partidas tienen id, ya que pueden repetirse nombres)
+          otro problema es que pueden poner ellos en el path del navegador por ejemplo ".../SalaDeEspera/4" y puede estar llena, lo que si, supongo que
+          no va a poder hacer nada ni va a contar como jugador para la partida, lo que me causa mas temor es que pueda toquetear cosas que no deba, pero
+          capaz se puede hacer lo suficientemente robusto como para que no pueda iniciar la partida ni jugar nada ni ver cartas que no deba, etc. */}
+      <Route path="/SalaDeEspera/:id">
+        <WaitRoom players_data={players_list} />
+      </Route>
+    </div>
   )
 }
 
-export default App
+export default App;
