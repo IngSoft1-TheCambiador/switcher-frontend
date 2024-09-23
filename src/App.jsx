46cdee1e7f3d0230183b06a256f6ab3bba220883
alvinary <49176889+alvinary@users.noreply.github.com>
2024-09-22 21:27:52 -0300

    Set up context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useContext } from 'react';
+import './App.css';
+import './index.css';
+import Inicio from './components/Inicio';
+import ListaPartidas from './components/ListaPartidas';
+import GamesList from './components/GamesList';
+import { Route } from "wouter";
+import AppProvider, { AppContext } from './contexts/Context';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = () =>
+(
+  <div className="container">
+    <div className="row mt-5">
+      <div className="col-12">
+        <AppProvider>
+          <AppContext.Consumer>
+            {
+              ({ gameId, setGameId, playerName, setPlayerName, gamesList, setGamesList }) => 
+              (
+               <div className="app-container">
+                 <Route path="/">
+                   <Inicio setName={setPlayerName}/>
+                 </Route>
+                 <Route path="/ListaPartidas">
+                   { GamesList(gamesList) }
+                 </Route>
+               </div>
+               )
+            }
+          </AppContext.Consumer>
+        </AppProvider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  </div>
+)
 
-export default App
+export default App;
