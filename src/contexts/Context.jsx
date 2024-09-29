b2bdf7ffef260f4b9921d51094ab3020101f429b
Fran <francisco.jose.navarro@mi.unc.edu.ar>
2024-09-29 14:56:15 -0300

    added list_games fetch

--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -2,27 +2,18 @@ import React, { useState, useEffect } from "react";
 
 export const AppContext = React.createContext({});
 
-export const AppProvider = ({children}) => {
-    const [game, setGame] = useState ({
-        gameId: 0,
-        playerName: '',
-        gameName: '',
-        gamesList:
-        [{"gameName" : "lala", "minPlayers" : 2, "maxPlayers" : 4},
-         {"gameName" : "bbbbbbbbbbbbb", "minPlayers" : 4, "maxPlayers" : 4},
-         {"gameName" : "lala", "minPlayers" : 2, "maxPlayers" : 4},
-         {"gameName" : "bbbbbbbbbbbbb", "minPlayers" : 4, "maxPlayers" : 4},
-         {"gameName" : "lala", "minPlayers" : 2, "maxPlayers" : 4},
-         {"gameName" : "bbbbbbbbbbbbb", "minPlayers" : 4, "maxPlayers" : 4},
-         {"gameName" : "lala", "minPlayers" : 2, "maxPlayers" : 4},
-         {"gameName" : "bbbbbbbbbbbbb", "minPlayers" : 4, "maxPlayers" : 4},
-        ],
-    });
+export const AppProvider = ({ children }) => {
+  const [game, setGame] = useState({
+    gameId: 0,
+    playerName: '',
+    gameName: ''
+  });
+  const [ gamesList, setGamesList ] = useState([{"gameName" : "lala", "minPlayers" : 2, "maxPlayers" : 4}]);
 
-    return (
-      <AppContext.Provider 
-        value={{ game, setGame }} >
-          {children}
-      </AppContext.Provider>
+  return (
+    <AppContext.Provider
+      value={{ game, setGame, gamesList, setGamesList }} >
+      {children}
+    </AppContext.Provider>
   );
 };
