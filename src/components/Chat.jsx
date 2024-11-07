0e528ae8e1a528c99cda309086f6ca2ca6e456f8
Matteo Mariano <matteomariano10@mi.unc.edu.ar>
2024-11-06 21:26:01 -0300

    Added use of send_message endpoint and add new messages to all players

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,13 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import './Chat.css'
 import { AppContext } from "../App";
+import { POST, httpRequest } from "../services/HTTPServices";
 
 function Chat({playerNames, playerColors}) {
-    const {clientId} = useContext(AppContext);
+    const {clientId, lastMessage, gameId} = useContext(AppContext);
     const [mensajes, setMensajes] = useState([]);
     const [tempMsg, setTempMsg] = useState('');
-    const now = new Date();
-    const hours = now.getHours();
-    const min = now.getMinutes();
+
 
     function parseColor(color) {
         if (color === "r") return "#ff5757";
@@ -18,15 +17,31 @@ function Chat({playerNames, playerColors}) {
         else if (color === "log") return "rgba(130, 130, 130, 0.6)";
     }
 
-    // TODO:
-        // implementar uso de useEffect para recibir mensajes y actualizar front
+    async function sendMessage() {
+        const requestData = {
+            "method": POST,
+            "service": `send_message?game_id=${gameId}&sender_id=${clientId}&txt=${tempMsg}`
+        };
 
-    // TODO: implementar llamada a endpoint para enviar mensajes 
-        // (de momento esta funcion solo modifica el estado del cliente, para hacer pruebas sobre el estilo de los mensajes)
-    const sendMessage = (e) => {
+        await httpRequest(requestData);
+    }
+
+    // TODO: Agregar uso de endpoint para obtener los mensajes de la partida en la base de datos 
+        // (se usaria para cuando el jugador haga refresh o salga y vuelva a entrar a la partida)
+    
+    // TODO: Agregar logs
+    useEffect(() => {
+        console.log(lastMessage.data);
+        if (lastMessage.data.includes("message", "sender_id", "sender_name", "time")) {
+            let msgObj = JSON.parse(lastMessage.data)
+            setMensajes([...mensajes, {sender: msgObj.sender_name, color: playerColors[msgObj.sender_id], message: msgObj.message, time: msgObj.time}])
+        }
+    }, [lastMessage])
+
+    const sendMsg = (e) => {
         e.preventDefault();
         if (tempMsg !== '') {
-            setMensajes([...mensajes, {sender: playerNames[clientId], color: playerColors[clientId], message: tempMsg, time: `${hours}:${min}`}]);
+            sendMessage();
             setTempMsg('');
         }
     }
@@ -48,9 +63,9 @@ function Chat({playerNames, playerColors}) {
             </div>
             {/* campo para escribir */}
             <form>
-                <input type="text" placeholder="Type something..." value={tempMsg} onChange={e => setTempMsg(e.target.value)} className="msg-textbox">
+                <input type="text" placeholder="Type something..." value={tempMsg} onChange={e => setTempMsg(e.target.value)} className="msg-textbox" maxLength={100}>  
                 </input>  
-                <button onClick={sendMessage} style={{ display: 'none' }} aria-hidden="true" />
+                <button onClick={sendMsg} style={{ display: 'none' }} aria-hidden="true" />
             </form>
         </>
     );
