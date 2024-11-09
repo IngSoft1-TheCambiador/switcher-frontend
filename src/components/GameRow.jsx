e0046d29f7270644b04e2bd99692a14ea0bf1758
Eduardo Ferré <eduardo.ferre@mi.unc.edu.ar>
2024-11-09 15:55:20 -0300

    create and join private games

--- src/components/GameRow.jsx
+++ src/components/GameRow.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../App.jsx';
 import { useLocation } from 'wouter';
 import '../index.css';
@@ -8,24 +8,33 @@ import { POST, httpRequest } from '../services/HTTPServices.jsx';
 function GameRow({ gameID, gameName, minPlayers, maxPlayers }) {
   const [, navigate] = useLocation();
   const { socketId, playerName, handleNewPlayer } = useContext(AppContext);
+  const [password, setPassword] = useState('');
 
   const joinGame = async () => {
     const requestData = {
       "method": POST,
-      "service": `join_game/?socket_id=${socketId}&game_id=${gameID}&player_name=${playerName}`
+      "service": `join_game/?socket_id=${socketId}&game_id=${gameID}&player_name=${playerName}&password=${password}`
     };
     const response = await httpRequest(requestData);
     if (response.json.response_status == 0) {
       handleNewPlayer(response.json.player_id, gameID);
       navigate('/WaitRoom');
+    } else {
+      alert('Contraseña incorrecta o error al unirse a la partida');
     }
   };
 
   return (
-    <div class='row-wrapper' onClick={joinGame}>
-      <div className="game-button">{gameName}</div>
+    <div className='row-wrapper'>
+      <div className="game-button" onClick={joinGame}>{gameName}</div>
       <div>{minPlayers}</div>
       <div>{maxPlayers}</div>
+      <input
+        type="password"
+        placeholder="Contraseña (opcional)"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
     </div>
   );
 }
